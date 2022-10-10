02e38bb82fb2b1c6d7211923241de3f4f9bf4f27
sayeef-rahman <sayeef.cse@gmial.com>
2022-10-10 18:27:10 +0600

    nodu modules added

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("hello");
+var num1 = '2';
+var num2 = '3';
+console.log(typeof (num1));
+console.log(+num1 + +num2);
---
+++ index.ts
@@ -0,0 +1,5 @@
+console.log("hello");
+const num1 = '2';
+const num2 = '3';
+console.log(typeof (num1));
+console.log(+num1 + +num2);
